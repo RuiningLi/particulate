30390aa1bbf85663e57b59333a70d3010a6c5d9e
YuxinYao620 <rosayaoyx@gmail.com>
2026-04-01 15:02:09 +0100

    fix eval

--- evaluate.py
+++ evaluate.py
@@ -10,8 +10,6 @@ from tqdm import tqdm
 
 from particulate.data_utils import load_obj_raw_preserve
 from particulate.evaluation_utils import evaluate_articulate_result
-import glob
-import os
 
 @torch.no_grad()
 @torch.autocast(device_type='cuda', dtype=torch.bfloat16)
